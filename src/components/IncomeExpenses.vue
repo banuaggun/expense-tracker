3063fc9ff557d9337a93702d75f848d332a0f0fd
banuaggun <banuaggun@outlook.com>
2023-12-26 17:10:01 +0300

    Bug Fixed

--- src/components/IncomeExpenses.vue
+++ src/components/IncomeExpenses.vue
@@ -1,13 +1,13 @@
 <template>
     <div>
         <h3>Income</h3>
-        <p>+${{ income }}</p>
+        <p>${{ income }}</p>
 
         <h3>Expenses</h3>
-        <p>-${{ expense }}</p>
+        <p>${{ expenses }}</p>
     </div>
 </template>
-<script>
+<script setup>
 import { defineProps } from "vue";
 
 const props = defineProps({
