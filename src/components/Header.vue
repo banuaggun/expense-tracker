ec2c5bdc1fd778fcaf6798fc5bffaf3e29b40b8d
banuaggun <banuaggun@outlook.com>
2024-01-09 22:41:53 +0300

    Header style is being created

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,22 +1,28 @@
 <template>
   <div class="header">
-    <span>Expense Tracker</span>
+      <div class="header-content-left">
+        <span>Expense Tracker</span>
+      </div>
+      <div class="header-content-right">
+        <a href="https://github.com/banuaggun/expense-tracker" rel="noreferrer noopener" target="_blank" className="header-item social">
+          Project Repository
+        </a>
+        <a href="https://portfolio-banuaggun.vercel.app/" rel="noreferrer noopener" target="_blank" className="header-item social">
+          Portfolio
+        </a>
+      </div>
+  </div>
+  <div class="header-mobile">
+      <div class="header-mobile-content-top">
+        <span>Expense Tracker</span>
+      </div>
+      <div class="header-mobile-content-bottom">
+        <a href="https://github.com/banuaggun/expense-tracker" rel="noreferrer noopener" target="_blank" className="header-item social">
+          Project Repository
+        </a>
+        <a href="https://portfolio-banuaggun.vercel.app/" rel="noreferrer noopener" target="_blank" className="header-item social">
+          Portfolio
+        </a>
+      </div>
   </div>
 </template>
-
-<style scoped>
-.header{
-  border:1px solid green;
-  width:100%;
-  height:80px;
-  display:flex;
-  align-items:center;
-  justify-content: center;
-}
-.header span{
-  font-size:1.2rem;
-  color:#242424;
-  font-weight:700;
-  letter-spacing:0.8px;
-}
-</style>
